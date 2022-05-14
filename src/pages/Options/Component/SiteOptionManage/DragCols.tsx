cb4e956d074a591674c97b1a1925c183969800ec
vec <vec@torzo.club>
2022-05-15 02:51:03 +0800

    [add] remove animation

--- src/pages/Options/Component/SiteOptionManage/DragCols.tsx
+++ src/pages/Options/Component/SiteOptionManage/DragCols.tsx
@@ -1,10 +1,11 @@
 import { equals } from 'ramda'
-import React from 'react'
+import React, { useState } from 'react'
 import { Droppable, Draggable, DraggingStyle, NotDraggingStyle } from 'react-beautiful-dnd'
 import { SiteOption, SiteRow } from '../../../../options/site-matrix'
 import s from './DragCols.module.css'
 import SiteWindow, { SiteWindowFrame } from './SiteWindow'
 import plusSrc from './plus.svg'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 const getItemStyle = (
   isDragging: boolean,
@@ -28,6 +29,130 @@ export const getColListStyle = (isDraggingOver: boolean): React.CSSProperties =>
 
 type Pos = null | Readonly<[number, number]>
 
+function SiteOptionDragItem(props: {
+  option: SiteOption
+  isEditMode: boolean
+  edit: Pos
+  colNum: number
+  rowNum: number
+  onCancelEdit(): void
+  onChange(id: SiteOption['id'], s: SiteOption): void
+  onSubmitEdit(colNum: number, s: SiteOption): void
+  onClickEdit(colNum: number): void
+  onClickRemove(colNum: number): void
+}) {
+  const { rowNum, colNum } = props
+  return (
+    <div className={s.SiteOption}>
+      <Draggable
+        key={`${props.option.id}`}
+        draggableId={`${props.option.id}`}
+        index={props.colNum}
+        isDragDisabled={Boolean(props.edit)}
+      >
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            className={s.ColDnDFrame}
+            style={getItemStyle(
+              snapshot.isDragging,
+              provided.draggableProps.style
+            )}
+          >
+            <SiteWindow
+              isEdit={equals(props.edit, [rowNum, colNum])}
+              isBlur={props.isEditMode && !equals(props.edit, [rowNum, colNum])}
+              onCancelEdit={props.onCancelEdit}
+              siteOption={props.option}
+              onChange={props.onChange}
+              onSubmit={(newOption) => {
+                props.onSubmitEdit(colNum, newOption)
+              }}
+              onClickEdit={() => {
+                props.onClickEdit(colNum)
+              }}
+              onClickRemove={() => {
+                props.onClickRemove(colNum)
+              }}
+            />
+          </div>
+        )}
+      </Draggable>
+    </div>
+  )
+}
+
+function SiteOptionList(props: {
+  enableRemoveAnimation: boolean
+  cols: SiteOption[]
+  isEditMode: boolean
+  edit: Pos
+  rowNum: number,
+  onCancelEdit(): void
+  onChange(id: SiteOption['id'], s: SiteOption): void
+  onSubmitEdit(colNum: number, s: SiteOption): void
+  onClickEdit(colNum: number): void
+  onClickRemove(colNum: number): void
+  onRemoveAnimationEnd(): void
+}) {
+  if (props.enableRemoveAnimation) {
+    return (
+      <TransitionGroup
+        className={s.SiteOptionList}
+      >
+        {props.cols.map((col, colNum) => (
+          <CSSTransition
+            onExited={props.onRemoveAnimationEnd}
+            key={col.id}
+            timeout={382}
+            classNames={{
+              enter: s.SiteOptionEnter,
+              enterActive: s.SiteOptionEnterActive,
+              enterDone: s.SiteOptionEnterDone,
+              exit: s.SiteOptionExit,
+              exitActive: s.SiteOptionExitActive,
+              exitDone: s.SiteOptionExitDone,
+            }}
+          >
+            <SiteOptionDragItem
+              option={col}
+              isEditMode={props.isEditMode}
+              edit={props.edit}
+              colNum={colNum}
+              rowNum={props.rowNum}
+              onCancelEdit={props.onCancelEdit}
+              onChange={props.onChange}
+              onSubmitEdit={props.onSubmitEdit}
+              onClickEdit={props.onCancelEdit}
+              onClickRemove={props.onClickRemove}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    )
+  } else {
+    return <>
+      {props.cols.map((col, colNum) => (
+        <SiteOptionDragItem
+          key={col.id}
+          option={col}
+          isEditMode={props.isEditMode}
+          edit={props.edit}
+          colNum={colNum}
+          rowNum={props.rowNum}
+          onCancelEdit={props.onCancelEdit}
+          onChange={props.onChange}
+          onSubmitEdit={props.onSubmitEdit}
+          onClickEdit={props.onCancelEdit}
+          onClickRemove={props.onClickRemove}
+        />
+      ))}
+    </>
+  }
+}
+
 export default function Cols(props: {
   rowNum: number,
   row: SiteRow,
@@ -40,6 +165,7 @@ export default function Cols(props: {
   onClickEdit(colNum: number): void
   onClickRemove(colNum: number): void
 }) {
+  const [enableRemoveAnimation, setEnableRemoveAnimation] = useState(false)
   const { row, rowNum } = props
 
   return (
@@ -49,56 +175,36 @@ export default function Cols(props: {
           ref={provided.innerRef}
           style={getColListStyle(snapshot.isDraggingOver)}
         >
-          {row.map((col, colNum) => {
-            return (
-              <Draggable
-                key={`${col.id}`}
-                draggableId={`${col.id}`}
-                index={colNum}
-                isDragDisabled={Boolean(props.edit)}
-              >
-                {(provided, snapshot) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className={s.ColDnDFrame}
-                    style={getItemStyle(
-                      snapshot.isDragging,
-                      provided.draggableProps.style
-                    )}
-                  >
-                    <SiteWindow
-                      isEdit={equals(props.edit, [props.rowNum, colNum])}
-                      isBlur={props.isEditMode && !equals(props.edit, [props.rowNum, colNum])}
-                      onCancelEdit={props.onCancelEdit}
-                      siteOption={col}
-                      onChange={props.onChange}
-                      onSubmit={(newOption) => {
-                        props.onSubmitEdit(colNum, newOption)
-                      }}
-                      onClickEdit={() => {
-                        props.onClickEdit(colNum)
-                      }}
-                      onClickRemove={() => {
-                        props.onClickRemove(colNum)
-                      }}
-                    />
-                  </div>
-                )}
-              </Draggable>
-            )
-          })}
-          <SiteWindowFrame>
-            <img
-              className={s.AddSite}
-              src={plusSrc}
-              style={{ cursor: 'pointer', width: '48px', height: '48px' }}
-              alt="add site option"
-              onClick={props.onClickAdd}
-            />
-          </SiteWindowFrame>
+          <SiteOptionList
+            enableRemoveAnimation={enableRemoveAnimation}
+            cols={row}
+            isEditMode={props.isEditMode}
+            edit={props.edit}
+            rowNum={rowNum}
+            onCancelEdit={props.onCancelEdit}
+            onChange={props.onChange}
+            onSubmitEdit={props.onSubmitEdit}
+            onClickEdit={props.onClickEdit}
+            onRemoveAnimationEnd={() => setEnableRemoveAnimation(false)}
+            onClickRemove={(colNum) => {
+              setEnableRemoveAnimation(true)
+              setTimeout(() => props.onClickRemove(colNum))
+            }}
+          />
+
           {provided.placeholder}
+
+          <div style={{ margin: '0 8px' }}>
+            <SiteWindowFrame>
+              <img
+                className={s.AddSite}
+                src={plusSrc}
+                style={{ cursor: 'pointer', width: '48px', height: '48px' }}
+                alt="add site option"
+                onClick={props.onClickAdd}
+              />
+            </SiteWindowFrame>
+          </div>
         </div>
       )}
     </Droppable>
