323d5f926aa418e5472ca74ddf1f9e6470b3f40a
vec <vec@torzo.club>
2022-05-23 04:47:13 +0800

    [add] src/options/version comment

--- src/options/versions/update.ts
+++ src/options/versions/update.ts
@@ -2,6 +2,8 @@ import { AllVersion, LatestVersion } from '.'
 import { updater as v2Updater } from './v2'
 import { updater as v3Updater } from './v3'
 
+// -------- 每次添加新的版本后，都得修改这块地方 --------
+// -------- 隔壁 index.ts 也要更新               --------
 export function updateOptions(s_opts: AllVersion): LatestVersion {
   switch (s_opts.version) {
     case 1:
