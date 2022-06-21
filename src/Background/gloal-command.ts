2cb43a2c254be156283295ce54f59a26f703366f
vec <vec@torzo.club>
2022-06-21 09:32:26 +0800

    [improve] update code comment

--- src/Background/gloal-command.ts
+++ src/Background/gloal-command.ts
@@ -3,22 +3,20 @@ import { controlIsLaunched } from '../x-state/control-window-launched'
 import launchControlWindow from './launch'
 
 // 未启动Poker的控制窗时候，快捷键 focus-layout 为启动 Poker 控制窗
-// 在启动控制窗后，快捷键 focus-layout 就不再是 Poker 控制窗了，
-// 而是原本的切换到搜索窗和控制窗的快捷键
-// 在控制窗关闭后，快捷键 focus-layout 又会激活为启动 Poker 控制窗
+// 在启动控制窗后，快捷键 focus-layout 不进行任何操作
+// 因为这时候，控制窗监听着 focus-layout 的快捷键，用于唤回 Poker Layout
 export default function GlobalCommand() {
   const [applyGlobalCommand, cancelGlobalCommand] = ChromeEvent(
     chrome.commands.onCommand,
     async (command) => {
       if (command === 'focus-layout') {
-        const is_launched = await controlIsLaunched()
-        if (!is_launched) {
+        if (await controlIsLaunched()) {
+          console.error('control window is launched')
+        } else {
           launchControlWindow({
             text: undefined,
             revert_container_id: undefined
           })
-        } else {
-          console.warn('control window is launched')
         }
       }
     }
