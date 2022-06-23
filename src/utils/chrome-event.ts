389ef123037ba1f6ddbab274ea23320ffd943b1c
vec <vec@torzo.club>
2022-06-23 12:19:26 +0800

    [add] utils/chrome-event repeat call applyEvent/cancelEvent checking

--- src/utils/chrome-event.ts
+++ src/utils/chrome-event.ts
@@ -1,10 +1,34 @@
+import { Memo } from 'vait'
+
+export class ChromeEventError extends Error {}
+
 export const ChromeEvent = <Fn extends Function>(
   ev: chrome.events.Event<Fn>,
   processFn: Fn
-) => [
-  () => ev.addListener(processFn),
-  () => ev.removeListener(processFn)
-] as const
+) => {
+  const [ isApplied, setApply ] = Memo(false)
+  return [
+    function applyEvent() {
+      if (isApplied()) {
+        throw new ChromeEventError('cannot apply chrome-event because Listener is Applied')
+      } else {
+        setApply(true)
+        ev.addListener(processFn)
+      }
+    },
+
+    function cancelEvent() {
+      if (isApplied()) {
+        setApply(false)
+        ev.removeListener(processFn)
+      } else {
+        console.warn(
+          new ChromeEventError('cannot cancel chrome-event because Listener is canceled')
+        )
+      }
+    }
+  ] as const
+}
 
 export function ApplyChromeEvent<Fn extends Function>(
   ev: chrome.events.Event<Fn>,
