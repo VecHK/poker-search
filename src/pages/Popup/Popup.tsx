d47fdd28b5c2f2a323a3ff86b9ceafa067013cc8
vec <vec@torzo.club>
2022-06-25 01:08:32 +0800

    [add] if controlIsLaunched is true, click Popup will sendMessage('Refocus')

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,5 +1,5 @@
 import { Atomic } from 'vait'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { controlIsLaunched } from '../../x-state/control-window-launched'
 import { sendMessage } from '../../message'
@@ -28,6 +28,17 @@ function AppMain() {
   const [input, setInput] = useState('')
   const current_window_id = useCurrentWindow()?.windowId
   const windowIsFocus = useWindowFocus(true)
+
+  useEffect(() => {
+    controlIsLaunched()
+      .then(isLaunched => {
+        if (isLaunched) {
+          sendMessage('Refocus', null).finally(() => {
+            window.close()
+          })
+        }
+      })
+  }, [])
 
   return (
     <main className="App-main">
