5b203295c5488108df7caee4fe4f054fa11b065a
vec <vec@torzo.club>
2022-06-28 12:24:36 +0800

    [add] add baidu try-poker inject

--- src/Content/index.ts
+++ src/Content/index.ts
@@ -8,12 +8,22 @@ function devLog(message?: any, ...optionalParams: any[]) {
 
 devLog('Poker Content Script works!')
 
+// type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+function insertAfter(newNode: Element, existingNode: Element) {
+  existingNode.parentNode?.insertBefore(newNode, existingNode.nextSibling)
+}
+
+function insertBefore(newNode: Element, currentNode: Element) {
+  currentNode.parentNode?.insertBefore(newNode, currentNode)
+}
+
 const $ = (s: string) => document.querySelector(s)
 
-function createTryNode(getSearchText: () => string, color = '#70757a') {
+function createTryNode(getSearchText: () => string, style: React.CSSProperties = {}) {
   const node = document.createElement('div')
   node.style.position = 'relative'
-  node.style.color = color
+  Object.assign(node.style, style)
   node.className = 'ext-try-poker'
   node.innerHTML = `找不到想要的结果？试试 <a style="cursor: pointer">Poker</a>`
 
@@ -75,6 +85,38 @@ const Series = [
         (extabar as unknown as any).style.height = '32px'
 
         botstuff.appendChild(createTryNode(getSearchText))
+      }
+    }
+  }),
+
+  InitInject({
+    name: 'Baidu',
+    cond() {
+      const u = new URL(window.location.href)
+      const is_baidu_hostname = u.hostname === 'www.baidu.com' || u.hostname === 'baidu.com'
+      const is_search_page = u.pathname === "/s"
+      const result_molecule_search_tool = Boolean( $('.result-molecule[tpl="app/search-tool"]') )
+      const result_molecule_page = Boolean( $('.result-molecule[tpl="app/page"]') )
+
+      return is_baidu_hostname && is_search_page && result_molecule_search_tool && result_molecule_page
+    },
+    async exec() {
+      const result_molecule_search_tool = $('.result-molecule[tpl="app/search-tool"]')
+      const result_molecule_page = $('.result-molecule[tpl="app/page"]')
+
+      if (!result_molecule_search_tool || !result_molecule_page) {
+        return
+      } else {
+        const getSearchText = () => {
+          const input = document.querySelector<HTMLInputElement>('input#kw')
+          if (input === null) {
+            throw Error('input is null')
+          } else {
+            return input.value
+          }
+        }
+        insertAfter(createTryNode(getSearchText, { color: '#9195a3' }), result_molecule_search_tool)
+        insertBefore(createTryNode(getSearchText, { color: '#9195a3', paddingLeft: '150px' }), result_molecule_page)
       }
     }
   })
