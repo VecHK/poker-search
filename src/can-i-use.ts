4c4814a0803cf8b9cb60ef9515690c20f68ce99b
vec <vec@torzo.club>
2022-07-01 02:33:41 +0800

    [add] canUseMinimizeRevert

--- src/can-i-use.ts
+++ src/can-i-use.ts
@@ -1,7 +1,7 @@
 import { Base } from './core/base'
 
 const isWindowsOS = (platform: Base['platform']) => platform.os === 'win'
-// const isMacOS = (platform: Base['platform']) => platform.os === 'mac'
+const isMacOS = (platform: Base['platform']) => platform.os === 'mac'
 
 const InitCanIUse = <A extends any[]>(
   fn: (...args: A) => boolean
@@ -19,4 +19,9 @@ const InitCanIUse = <A extends any[]>(
 // 唤回窗只有 Windows 系统才有，而且要开启 【「唤回 Poker」窗口】 的设置项
 export const [ canUseRefocusWindow, CanUseRefocusWindow ] = InitCanIUse(
   (platform: Base['platform']) => isWindowsOS(platform)
+)
+
+// 利用搜索窗的最小化来还原窗口只有 macOS 系统才有
+export const [ canUseMinimizeRevert, CanUseMinimizeRevert ] = InitCanIUse(
+  (platform: Base['platform']) => isMacOS(platform)
 )
