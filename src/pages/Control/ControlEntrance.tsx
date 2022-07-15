14612b6b77e1bbf700ef77324f64bdb7f799a7e5
vec <vec@torzo.club>
2022-07-15 22:59:08 +0800

    [add] filtered floor tips

--- src/pages/Control/ControlEntrance.tsx
+++ src/pages/Control/ControlEntrance.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { useMemo } from 'react'
+import { useEffect } from 'react'
 import cfg from '../../config'
 import { Base, createBase, RevertContainerID } from '../../core/base'
 import getQuery from '../../utils/get-query'
@@ -21,25 +23,57 @@ export function getRevertContainerId(): RevertContainerID {
   }
 }
 
+function useFilteredFloorTips() {
+  const [show_tips, setTips] = useState<number | false>(false)
+
+  useEffect(() => {
+    if (show_tips !== false) {
+      const handler = setTimeout(() => {
+        setTips(() => false)
+      }, 3000)
+      return () => clearTimeout(handler)
+    }
+  }, [show_tips])
+
+  return [
+    function showTips() {
+      setTips(Date.now())
+    },
+
+    useMemo(() => (
+      <div className={`filtered-floor-tips ${show_tips ? 'show' : ''}`}>
+        你可能需要重新提交关键字才能应用新选择的层
+      </div>
+    ), [show_tips])
+  ] as const
+}
+
 export default function Entrance({
   base: firstBase
 }: { base: Base }) {
   const [base, setBase] = useState<Base>(firstBase)
 
+  const [showTips, tips_node] = useFilteredFloorTips()
+
   return (
-    <Control
-      base={base}
-      onSelectedFloorChange={(selected_idx_list) => {
-        const s_ids = base.preferences.site_settings.map(s => s.id)
-        const filtered_floor = s_ids.filter((_, idx) => {
-          return selected_idx_list.indexOf(idx) === -1
-        })
-        saveFilteredFloor(filtered_floor).then(() => {
-          createBase(getRevertContainerId()).then((base) => {
-            setBase(base)
+    <>
+      <Control
+        base={base}
+        onSelectedFloorChange={(selected_idx_list) => {
+          const s_ids = base.preferences.site_settings.map(s => s.id)
+          const filtered_floor = s_ids.filter((_, idx) => {
+            return selected_idx_list.indexOf(idx) === -1
           })
-        })
-      }}
-    />
+          saveFilteredFloor(filtered_floor).then(() => {
+            showTips()
+            createBase(getRevertContainerId()).then((base) => {
+              setBase(base)
+            })
+          })
+        }}
+      />
+
+      {tips_node}
+    </>
   )
 }
