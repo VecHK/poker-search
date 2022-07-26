2305f6afb7cb0d964d09ebc83f17c659c98e0232
vec <vec@torzo.club>
2022-07-27 03:19:14 +0800

    [fix] launch position failure

--- src/Background/modules/launch.ts
+++ src/Background/modules/launch.ts
@@ -5,6 +5,31 @@ import { getControlWindowHeight } from '../../core/base/control-window-height'
 import { calcControlWindowPos } from '../../core/layout/control-window'
 import { controlIsLaunched, setControlLaunch } from '../../x-state/control-window-launched'
 
+async function controlBounds() {
+  const base = await createBase(undefined)
+  const site_settings = selectSiteSettingsByFiltered(
+    base.preferences.site_settings,
+    base.init_filtered_floor
+  )
+
+  const info = createLayoutInfo(base.environment, base.limit, site_settings)
+
+  const control_window_height = getControlWindowHeight(site_settings)
+
+  const [ top, left ] = calcControlWindowPos(
+    control_window_height,
+    info.total_height,
+    base.limit
+  )
+
+  return {
+    top,
+    left,
+    width: cfg.CONTROL_WINDOW_WIDTH,
+    height: control_window_height,
+  } as const
+}
+
 export const getControlWindowUrl = thunkify(chrome.runtime.getURL)(
   `/control.html`
 )
@@ -29,46 +54,23 @@ function generateUrl({ text, revert_container_id }: {
   }
 }
 
-async function getControlPos() {
-  const base = await createBase(undefined)
-  const info = createLayoutInfo(base.environment, base.limit, base.preferences.site_settings)
-
-  const site_settings = selectSiteSettingsByFiltered(
-    base.preferences.site_settings,
-    base.init_filtered_floor
-  )
-
-  const [ top, left ] = calcControlWindowPos(
-    getControlWindowHeight(site_settings),
-    info.total_height,
-    base.limit
-  )
-  return [ top, left ] as const
-}
-
 export default async function launchControlWindow({ text, revert_container_id }: {
   text: string | undefined
   revert_container_id: RevertContainerID
 }) {
-  const base_P = createBase(undefined)
-  const control_is_launched_P = controlIsLaunched()
-  if (await control_is_launched_P) {
+  if (await controlIsLaunched()) {
     throw Error('control window is Launched')
   } else {
-    const site_settings = selectSiteSettingsByFiltered(
-      (await base_P).preferences.site_settings,
-      (await base_P).init_filtered_floor
-    )
-    const control_window_height = getControlWindowHeight(site_settings)
-    const [ top, left ] = await getControlPos()
+    const { top, left, height, width } = await controlBounds()
+
     const controlWindow = await chrome.windows.create({
       url: generateUrl({ text, revert_container_id }),
       type: 'popup',
       state: 'normal',
       focused: true,
 
-      width: Math.round(cfg.CONTROL_WINDOW_WIDTH),
-      height: Math.round(control_window_height),
+      width: Math.round(width),
+      height: Math.round(height),
       left: Math.round(left),
       top: Math.round(top),
     })
