a150e29087e2b98b4b195fe929624edec93ea63c
vec <vec@torzo.club>
2022-07-26 21:43:21 +0800

    [improve] improve trimOne

--- src/utils/match-search-pattern.ts
+++ src/utils/match-search-pattern.ts
@@ -2,15 +2,22 @@ import { nth, pipe, slice } from 'ramda'
 
 type MatchResult = Readonly<[true, string, string] | [false]>
 
+const blankCharExp = / |　/g
+
 function trimOne(str: string) {
-  const [, ...remain] = str
-  return remain.join('')
+  const [ first, ...remain ] = str
+
+  if (blankCharExp.test(first)) {
+    return remain.join('')
+  } else {
+    return str
+  }
 }
 
 export default function matchSearchPattern(
   input_str: string
 ): MatchResult {
-  const found = input_str.matchAll(/ |　/g)
+  const found = input_str.matchAll(blankCharExp)
   const match = nth(0, [...found])
 
   if (match) {
