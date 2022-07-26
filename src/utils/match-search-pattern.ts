e9a623ddd337ad18945e56bd2ef79c85e3cd63d2
vec <vec@torzo.club>
2022-07-26 21:39:08 +0800

    [adjust] trimStart instead of trimOne

--- src/utils/match-search-pattern.ts
+++ src/utils/match-search-pattern.ts
@@ -1,6 +1,11 @@
-import { nth, slice } from 'ramda'
+import { nth, pipe, slice } from 'ramda'
 
 type MatchResult = Readonly<[true, string, string] | [false]>
+
+function trimOne(str: string) {
+  const [, ...remain] = str
+  return remain.join('')
+}
 
 export default function matchSearchPattern(
   input_str: string
@@ -11,15 +16,19 @@ export default function matchSearchPattern(
   if (match) {
     const { index } = match
     if (index !== undefined) {
-      return [
-        true,
-        slice(0, index, input_str),
-        slice(index, input_str.length, input_str).trimStart()
-      ] as const
+      const [ getLeft, getRight ] = [
+        slice(0, index),
+        pipe(
+          slice(index, input_str.length),
+          trimOne
+        )
+      ]
+
+      return [ true, getLeft(input_str), getRight(input_str) ]
     } else {
-      return [ false ] as const
+      return [ false ]
     }
   } else {
-    return [ false ] as const
+    return [ false ]
   }
 }
