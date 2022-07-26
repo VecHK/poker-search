d126d27afe4015c67e24bb735953e3b879810503
vec <vec@torzo.club>
2022-07-26 17:28:30 +0800

    [adjust] 'DEFAULT_SITE_OPTION_NAME' -> 'DEFAULT_SITE_SETTING_FLOOR_NAME'

--- src/config/index.ts
+++ src/config/index.ts
@@ -53,7 +53,7 @@ const cfg = Object.freeze({
   },
 
   EXPORT_SITE_SETTINGS_FILE_NAME: 'poker-sites.json',
-  DEFAULT_SITE_OPTION_NAME: '站点',
+  DEFAULT_SITE_SETTING_FLOOR_NAME: '',
 
   MOBILE_USER_AGNET: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
 
